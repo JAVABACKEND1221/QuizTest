1655e06af8d9ccd0d6f855e19f346e39807a3672
OTABEK <otabekkenjayev.002@gmail.com>
2022-01-21 23:28:51 +0500

    Initial commit

---
+++ src/main/java/bestteam/quiztest/model/AppUserRole.java
@@ -0,0 +1,13 @@
+package bestteam.quiztest.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum AppUserRole implements GrantedAuthority {
+  ROLE_ADMIN,
+  ROLE_USER;
+
+  public String getAuthority() {
+    return name();
+  }
+
+}
---
+++ src/main/java/bestteam/quiztest/model/Questions.java
@@ -0,0 +1,7 @@
+package bestteam.quiztest.model;
+
+import lombok.Data;
+
+@Data
+public class Questions {
+}
